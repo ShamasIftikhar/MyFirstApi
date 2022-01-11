24c0c563730e891293597519c67f5a791ad42f1c
Shamas <servingforothers@gmail.com>
2022-01-11 20:37:31 +0500

    Trying still

--- app/config.py
+++ app/config.py
@@ -3,13 +3,13 @@ from pydantic import BaseSettings
 
 class Setting(BaseSettings):
     database_hostname:str
-    database_port:int
+    database_port:str
     database_password:str
     database_name:str
     database_username:str
     secret_key:str
     algorithm:str
-    access_token_expiration_minutes:int
+    access_token_expiration_minutes:str
     
     class Config:
         env_file= ".env"
