8cd61b70570ca9af3f34863707b22757939defd1
Shamas <servingforothers@gmail.com>
2022-01-12 17:30:16 +0500

    testing again docker

--- app/routers/users.py
+++ app/routers/users.py
@@ -7,8 +7,10 @@ router =APIRouter(prefix="/users",tags=['Users'])
 
 @router.post('/',status_code=status.HTTP_201_CREATED,response_model=schemas.UserResponse)
 def create_user(user:schemas.Users,db: Session = Depends(get_db)):
+    res= db.query(models.Post).filter(user.email==models.User.email).first()
+    if res:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT,detail=f'Email "{user.email}"already exist!')
     user.password = utils.hash(user.password)
-    
     new_user=models.User(**user.dict())
     db.add(new_user)
     db.commit()
